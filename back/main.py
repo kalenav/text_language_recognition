4538641a1ceefe6fade84cadb3b149d6673b4b99
Polina <polinavinograd40@gmail.com>
2023-11-09 02:12:22 +0300

    added class of RecognizerFactory

--- back/main.py
+++ back/main.py
@@ -6,16 +6,34 @@ import utils
 from path import LOCAL_PATH
 
 
+class RecognizerFactory:
+    _recognizers = {}
+
+    @staticmethod
+    def get_recognizer(method_type):
+        if method_type not in RecognizerFactory._recognizers:
+            if method_type == 'neural_network':
+                RecognizerFactory._recognizers[method_type] = NeuralNetworkMethod()
+            elif method_type == 'frequency_words':
+                RecognizerFactory._recognizers[method_type] = FrequencyWordsMethod()
+            elif method_type == 'alphabet':
+                RecognizerFactory._recognizers[method_type] = AlphabetMethod()
+            else:
+                raise ValueError("Unsupported method type")
+
+        return RecognizerFactory._recognizers[method_type]
+
+
 def recognize_lang(document: Document, method: int) -> str:
     if method == 1:
-        RECOGNIZER = AlphabetMethod()
+        RECOGNIZER = RecognizerFactory.get_recognizer(method_type="alphabet")
         return RECOGNIZER.find_language(RECOGNIZER.get_local_symbol_frequency(document))
 
     elif method == 2:
-        RECOGNIZER = FrequencyWordsMethod()
+        RECOGNIZER = RecognizerFactory.get_recognizer(method_type="frequency_words")
         return RECOGNIZER.find_language(RECOGNIZER.get_local_word_frequency(document))
     else:
-        RECOGNIZER = NeuralNetworkMethod()
+        RECOGNIZER = RecognizerFactory.get_recognizer(method_type="neural_network")
         return RECOGNIZER.predict_language(document)
 
 
