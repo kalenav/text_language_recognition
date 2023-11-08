5edffc51120f170da4e3b62b1d172246ae5ad261
Polina <polinavinograd40@gmail.com>
2023-11-09 02:11:09 +0300

    changed ML-classifying to NN-classifying(Multi Layer Perseptron)

--- back/neural_network_method.py
+++ back/neural_network_method.py
@@ -1,5 +1,5 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neural_network import MLPClassifier
 import utils
 from document import Document
 from path import LOCAL_PATH
@@ -7,17 +7,16 @@ from path import LOCAL_PATH
 
 class NeuralNetworkMethod:
     def __init__(self):
-        self.__vectorizer = CountVectorizer()
-        self.__classifier = MultinomialNB()
+        labels = [*["eng"] * 5, *["rus"] * 5]
+        self.__label_dict = {label: idx for idx, label in enumerate(set(labels))}
+        training_texts = [document.text for document in
+                          utils.get_all_documents_in_folder(LOCAL_PATH + "\\documents\\training_documents")]
 
-        self.__labels = [*["eng"]*5, *["rus"]*5]
-        self.__label_dict = {label: idx for idx, label in enumerate(set(self.__labels))}
-        self.__training_texts = [document.text for document in
-                               utils.get_all_documents_in_folder(LOCAL_PATH + "\\documents\\training_documents")]
+        self.__vectorizer = TfidfVectorizer()
+        training_input = self.__vectorizer.fit_transform(training_texts)
+        training_output = [self.__label_dict[label] for label in labels]
 
-        # training bayes classifier
-        training_input = self.__vectorizer.fit_transform(self.__training_texts)
-        training_output = [self.__label_dict[label] for label in self.__labels]
+        self.__classifier = MLPClassifier()
         self.__classifier.fit(training_input, training_output)
 
     def predict_language(self, document: Document) -> str:
